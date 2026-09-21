47681b34672fec4e8e0d2579de0182ed9c55de3b
ActuallyFlamey <flamey@hexa.blue>
2026-09-21 22:06:24 +0200

    prisma & better-auth init

---
+++ src/lib/server/auth.ts
@@ -0,0 +1,27 @@
+import { betterAuth } from "better-auth";
+import { prismaAdapter } from "better-auth/adapters/prisma";
+import { prisma } from "../prisma";
+import { sveltekitCookies } from 'better-auth/svelte-kit';
+import { getRequestEvent } from "$app/server";
+
+export const auth = betterAuth({
+    baseURL: process.env.ORIGIN,
+    secret: process.env.BETTER_AUTH_SECRET,
+    database: prismaAdapter(prisma, {
+        provider: "postgresql",
+    }),
+    emailAndPassword: { enabled: true },
+    socialProviders: {
+        google: {
+            clientId: process.env.GOOGLE_CLIENT_ID as string,
+            clientSecret: process.env.GOOGLE_CLIENT_SECRET as string
+        },
+        github: {
+            clientId: process.env.GITHUB_CLIENT_ID as string,
+            clientSecret: process.env.GITHUB_CLIENT_SECRET as string
+        }
+    },
+    plugins: [
+        sveltekitCookies(getRequestEvent)
+    ]
+});
