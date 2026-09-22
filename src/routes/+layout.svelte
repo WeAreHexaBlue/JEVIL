17f66f4c941e13fbcbee58c7a9a748d6f0120e65
ActuallyFlamey <flamey@hexa.blue>
2026-09-22 10:36:04 +0200

    init a bunch of things

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,16 +1,25 @@
 <script lang="ts">
-	import type { Pathname } from '$app/types';
-	import { resolve } from '$app/paths';
-	import { page } from '$app/state';
-	import { locales, localizeHref } from '$lib/paraglide/runtime';
-	import './layout.css';
-	import favicon from '$lib/assets/favicon.svg';
+	import type { Pathname } from "$app/types";
+	import { resolve } from "$app/paths";
+	import { page } from "$app/state";
+	import { locales, localizeHref } from "$lib/paraglide/runtime";
+	import "./layout.css";
+	import favicon from "$lib/assets/favicon.png";
+
+	import Navigation from "$lib/components/navigation.svelte";
+	import Footer from "$lib/components/footer.svelte";
 
 	let { children } = $props();
 </script>
 
 <svelte:head><link rel="icon" href={favicon} /></svelte:head>
-{@render children()}
+<div class="flex flex-col min-h-screen">
+	<Navigation />
+	<div class="flex flex-col flex-1 pt-40">
+		{@render children()}
+	</div>
+	<Footer />
+</div>
 
 <div style="display:none">
 	{#each locales as locale (locale)}
